e5d666c296a0a5690a2a6bee0afe4ab4e5d8afd1
Nicole Gile <nicolegile94@gmail.com>
2020-06-21 19:22:31 -0500

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
 var startBtn = document.getElementById('start');
 var scoreEl = document.getElementById('score');
 
+var questions = [
+    {question: 'Which built-in method returns the calling string value converted to lower case?',
+    answers: ['A: toLower()', 'B: toLowerCase()', 'C: convertLowerCase()', 'D: convertLower()'],
+    correct: 1
+    },
+    {question: 'Inside which HTML elements do we put the Javascript?',
+    answers: ['A: <script>', 'B: <js>', 'C: <java>', 'D: none of the above'],
+    correct: 0
+    },
+    {question: 'What is the name of the web markup language?',
+    answers: ['A: python', 'B: HTML', 'C: JavaScript', 'D: CSS'],
+    correct: 3
+    },
+    {question: 'What year was JavaScript created?',
+    answers: ['A: 1993', 'B: 1994', 'C: 1995', 'D: 1996'],
+    correct: 2
+    },
+    {question: 'What is hte most popular coding language in the world?',
+    answers: ['A: python', 'B: HTML', 'C: JavaScript', 'D: CSS'],
+    correct: 2
+    }
+];
+
 function timer() {
     //initial time
     var timeLeft = 75;
